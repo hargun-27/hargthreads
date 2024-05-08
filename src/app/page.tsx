059dacb01bedc9761278fade12aae00b49afbc94
Hargun Sibal <hargunsibal@gmail.com>
2024-05-08 13:17:12 -0400

    fix feed pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
       <h1 className="font-bold text-3xl md:text-4xl">hargthread Feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/*@ts-expect-error server component*/}
-        {session ? <GeneralFeed /> : <GeneralFeed />}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-gray-950 py-4 px-6">
--- src/components/CustomFeed.tsx
+++ src/components/CustomFeed.tsx
@@ -1,28 +1,9 @@
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
-import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import PostFeed from "./PostFeed";
 
 const CustomFeed = async () => {
-  const session = await getAuthSession();
-
-  const followedCommunities = await db.subscription.findMany({
-    where: {
-      userId: session?.user.id,
-    },
-    include: {
-      subhargthread: true,
-    },
-  });
-
   const posts = await db.post.findMany({
-    where: {
-      subhargthread: {
-        name: {
-          in: followedCommunities.map((sub) => sub.subhargthread.name),
-        },
-      },
-    },
     orderBy: {
       createdAt: "desc",
     },
